e664041fe91a32beda31bedd0ce524b7f0ce6f8e
syaoyaoyao <syao0@chicagobooth.edu>
2023-01-28 20:33:32 +0000

    homework

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,49 @@
 class ApplicationController < ActionController::Base
+  def addition_result_page
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @sum = @first_num + @second_num
 
+    render({ :template => "math_templates/addition_result.html.erb" })
+  end
+
+  def addition_page
+    render({ :template => "math_templates/addition.html.erb" })
+  end
+
+  def subtract_result_page
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @diff = @first_num - @second_num
+
+    render({ :template => "math_templates/subtract_result.html.erb" })
+  end
+
+  def subtract_page
+    render({ :template => "math_templates/subtract.html.erb" })
+  end
+
+  def multiply_result_page
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num * @second_num
+
+    render({ :template => "math_templates/multiply_result.html.erb" })
+  end
+
+  def multiply_page
+    render({ :template => "math_templates/multiply.html.erb" })
+  end
+
+  def divide_result_page
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
+
+    render({ :template => "math_templates/divide_result_page.html.erb" })
+  end
+
+  def divide_page
+    render({ :template => "math_templates/divide_page.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/wizard_add", { :controller => "application", :action => "addition_result_page" })
+  get("/add", { :controller => "application", :action => "addition_page" })
+
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_result_page" })
+  get("/subtract", { :controller => "application", :action => "subtract_page" })
+
+  get("/wizard_multiply", { :controller => "application", :action => "multiply_result_page" })
+  get("/multiply", { :controller => "application", :action => "multiply_page" })
+
+  get("/wizard_divide", { :controller => "application", :action => "divide_result_page" })
+  get("/divide", { :controller => "application", :action => "divide_page" })
 end
